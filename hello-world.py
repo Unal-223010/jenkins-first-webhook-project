929668e5870fee5a88cedffb365d14ad987ca625
Unal-223010 <76978205+Unal-223010@users.noreply.github.com>
2022-01-17 13:48:04 +0100

    modified .py file

--- hello-world.py
+++ hello-world.py
@@ -5,7 +5,7 @@ print('j essayer pipeline-avec-jenkinsfile-and-webhook')
 print('Hello World for Jenkins Job')
 
 print('Github dan manuel degisim yapiyorum')
-
+print('ali')
 print("2+3")
 
 print(2+3)
